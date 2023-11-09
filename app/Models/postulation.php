ccdc9012ea133b583bc7eaa1f037b41bd74a4b8b
marwanlaouadi <marwanlaouadi@gmail.com>
2023-11-09 01:52:07 +0100

    marwan database

--- app/Models/postulation.php
+++ app/Models/postulation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class postulation extends Model
+class Postulation extends Model
 {
     use HasFactory;
 }
